9b11cb828dea2fe59158e1ac6fcddbd4418c113c
kikiAgustin <kikiagustin62@gmail.com>
2022-04-08 06:36:40 +0600

    beresin belajar fetch refactor dan even binding

--- 24. fetch refactor/script.js
+++ 24. fetch refactor/script.js
@@ -1,15 +1,83 @@
 
-const buttonSearch = document.querySelector('.button-search-movie');
-buttonSearch.addEventListener('click', function(){
-    const inputSearch = document.querySelector('.input-search-movie');
-    const movies = getMovies(inputSearch.value);
+
+// const buttonSearch = document.querySelector('.button-search-movie');
+// const inputSearch = document.querySelector('.input-search-movie');
+// buttonSearch.addEventListener('click', function () {
+//     fetch('http://www.omdbapi.com/?apikey=31ffb5af&s=' + inputSearch.value)
+//         .then(response => response.json())
+//         .then(response => {
+//             const movies = response.Search;
+//             let cards = '';
+//             movies.forEach(m => cards += showCard(m));
+//             const movieContainer = document.querySelector('.movie-container');
+//             movieContainer.innerHTML = cards;
+
+//             // Ketika tombol diklik
+//             const buttonDetail = document.querySelectorAll('.modal-detail-button');
+//             buttonDetail.forEach(btn => {
+//                 btn.addEventListener('click', function () {
+//                     const imdbid = this.dataset.imdbid;
+//                     fetch('http://www.omdbapi.com/?apikey=31ffb5af&i=' + imdbid)
+//                         .then(m => m.json())
+//                         .then(m => {
+
+//                             const modalDetail = document.querySelector('.modal-body');
+//                             modalDetail.innerHTML = showDetailMovie(m);
+//                         });
+//                 });
+
+//             });
+
+//         });
+// });
+
+
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', async function () {
+    const inputKeyword = document.querySelector('.input-keyword');
+    const movies = await getMovies(inputKeyword.value);
+    updateUI(movies);
 });
 
-function getMovies(keyword){
-    return fetch('http://www.omdbapi.com/?apikey=31ffb5af&s=' + keyword )
-        .then(response => response.json)
-        .then(response => response.search );
+
+// Even Binding
+document.addEventListener('click', async function (e) {
+
+    if (e.target.classList.contains('modal-detail-button')) {
+        const imdbid = e.target.dataset.imdbid;
+        const movieDetail = await getMovieDetail(imdbid);
+        updateMovieDetail(movieDetail);
+    }
+});
+
+function getMovieDetail(imdbid) {
+    return fetch('http://www.omdbapi.com/?apikey=31ffb5af&i=' + imdbid)
+        .then(m => m.json())
+        .then(m => m);
 }
+
+function updateMovieDetail(m) {
+    const modalBody = document.querySelector('.modal-body');
+    modalBody.innerHTML = showDetailMovie(m);
+}
+
+
+
+function getMovies(keyword) {
+    return fetch('http://www.omdbapi.com/?apikey=31ffb5af&s=' + keyword)
+        .then(response => response.json())
+        .then(response => response.Search);
+}
+
+function updateUI(movies) {
+
+    let card = '';
+    movies.forEach(m => card += showCard(m));
+    const cardMovie = document.querySelector('.movie-container');
+    cardMovie.innerHTML = card;
+}
+
+
 
 
 function showCard(m) {
